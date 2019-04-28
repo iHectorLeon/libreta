24c92fed00600d9262c7a149cc3ae045852c3e29
hectoraramirez <hector.leon.83@gmail.com>
2019-04-28 14:11:03 -0500

    version 1.0.5

--- src/app/user/mygroups/mygroups.component.ts
+++ src/app/user/mygroups/mygroups.component.ts
@@ -1,70 +1,76 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute } from '@angular/router';
-import { UserService } from './../../shared/sharedservices/user.service';
 import { CourseService } from './../../shared/sharedservices/course.service';
+import { UserService } from './../../shared/sharedservices/user.service';
 
 
 
 @Component({
   selector: 'app-mygroups',
   templateUrl: './mygroups.component.html',
-  providers:[UserService,CourseService]
+  providers: [UserService, CourseService]
 })
 export class MygroupsComponent implements OnInit {
 
-  public identiti;
-  public token;
+  identiti: any;
+  token: any;
+  cursoslist: any[] = [];
+  cursosinactive: any[] = [];
+  loading = false;
+  messageNewUser = false;
 
-  public cursoslist:any[]=[];
-  public loading:boolean = false;
-  public messageNewUser : boolean = false;
-
-  constructor(private _router:Router, private _activeRouter:ActivatedRoute, private _user:UserService, private _course:CourseService) {
-    this.identiti = this._user.getIdentiti();
-    this.token = this._user.getToken();
+  constructor(private router: Router, private user: UserService, private course: CourseService) {
+    this.identiti = this.user.getIdentiti();
+    this.token = this.user.getToken();
   }
 
   ngOnInit() {
-    this.loading = true
-    this.identiti = this._user.getIdentiti();
+    this.loading = true;
+    this.identiti = this.user.getIdentiti();
     this.getCourseUser();
   }
 
-  getCourseUser(){
-    this._course.getCourses(this.token).subscribe(data =>{
-      let mycursos = data.message.groups;
-      this._course.getCoursesOrg().subscribe(data=>{
-        for(let idcr of data.message.courses){
-          for(let idmg of mycursos){
-            if(idcr.id == idmg.courseid){
-              this.cursoslist.push({
-                curso: idmg,
-                imagen: idcr.image
-              })
+  getCourseUser() {
+    this.course.getCourses(this.token).subscribe(data => {
+      const mycursos = data.message.groups;
+      this.course.getCoursesOrg().subscribe( res => {
+        for (const idcr of res.message.courses) {
+          for (const idmg of mycursos) {
+            if (idcr.id === idmg.courseid ) {
+              if (idmg.status === 'active') {
+                this.cursoslist.push({
+                  curso: idmg,
+                  imagen: idcr.image
+                });
+              } else if (idmg.status === 'closed') {
+                this.cursosinactive.push({
+                  curso: idmg,
+                  imagen: idcr.image
+                });
+              }
             }
           }
         }
       });
       this.messageNewUser = false;
-      this.loading = false
-    },error=>{
-      if(error._body.includes('"message":"No groups found"')){
+      this.loading = false;
+    }, error => {
+      if (error._body.includes('"message":"No groups found"')) {
 
       }
       this.messageNewUser = error._body.includes('"message":"No groups found"');
-      this.loading = false
+      this.loading = false;
     });
   }
 
   /*
   Metodo para redireccionar al usuario al curso que selecciono
   */
-  public getMycourse(course:string, groupid:string, courseid:string, lastSeenBlock:string, firstBlock:string){
-    if(!lastSeenBlock){
-      this._router.navigate(['/mycourses',course,groupid,courseid,firstBlock]);
-    }else{
-      this._router.navigate(['/mycourses',course,groupid,courseid,lastSeenBlock]);
+  public getMycourse(course: string, groupid: string, courseid: string, lastSeenBlock: string, firstBlock: string) {
+    if (!lastSeenBlock) {
+      this.router.navigate(['/mycourses', course, groupid, courseid, firstBlock]);
+    } else {
+      this.router.navigate(['/mycourses', course, groupid, courseid, lastSeenBlock]);
     }
   }
-
 }
